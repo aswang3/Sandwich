1e91d1fcdd408631f6505fb8ee0d1869412c5acb
Pooja Welling <pgwelli2@linux-a2.ews.illinois.edu>
2016-10-12 09:43:40 -0500

    multiple users opening file supported

--- connect.c
+++ connect.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <wait.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
 
@@ -13,16 +15,32 @@ int main(int argc, char *argv[]) {
   FILE* f_main = fopen(argv[1], "r");
 
   // NEEDS: temp file is going to be in shared space, not current directory
-  FILE* f_temp = fopen("phantom_sandwich.c", "a+");
 
-  // NEEDS: should check if someone is already editing before trying to overwrite temp
+//mapping from input file to hidden file
+	char filehold [strlen(argv[1])+2];
+	strcpy(filehold, ".");
+	strcat(filehold, argv[1]);
 
-  char* txt;
-  size_t len = 0;
-  ssize_t line = 0;
-  while ((line = getline(&txt, &len, f_main)) != EOF) {
-    fprintf(f_temp, "%s", txt);
-  }
+
+//try to open the file. if it is null, create it
+
+  FILE* f_temp = fopen(filehold, "r+b");
+
+	if(!f_temp)
+	{
+		f_temp = fopen(filehold , "a+");
+
+		  // NEEDS: should check if someone is already editing before trying to overwrite temp
+
+		  char* txt;
+		  size_t len = 0;
+		  ssize_t line = 0;
+		  while ((line = getline(&txt, &len, f_main)) != EOF) {
+		  fprintf(f_temp, "%s", txt);
+	 	}
+
+	}
+
 
   fclose(f_main);
   fclose(f_temp);
@@ -33,7 +51,11 @@ int main(int argc, char *argv[]) {
   pid_t p = fork();
 
   if (!p) {
-    execlp("vim", "vim", "phantom_sandwich.c", (char*) NULL);
+    execlp("vim", "vim", filehold, (char*) NULL);
+//needs to pipe in automatic E to vim
+
+//	fprintf(stdin, "E\n", 3);
+
     printf("Cannot open temp file\n");
     exit(1);
   }
