392a68d9f7675d89537ba1eb14e3db07435fcaa6
local <localadmin@fa16-cs241-290.cs.illinois.edu>
2016-11-12 17:35:14 -0600

    try 2

--- send.c
+++ send.c
@@ -10,24 +10,34 @@
 ** Reads buffer from stdin and writes into FIFO pipe
 **/
 int main(){
-  char * myfifo = "/tmp/myfifo";
+  char* myfifo = "/tmp/pipe";
 
   mkfifo(myfifo, 0666);
   int fd = open(myfifo, O_WRONLY); // FIFO PIPE
 
-  char * line = NULL;
+  char* line = NULL;
   size_t len = 0;
 
   char* buf = malloc(0);
 
-  while(getline(&line, &len, stdin) != -1){
+  while (getline(&line, &len, stdin) != EOF) {
+    //printf("a\n");
     buf = realloc(buf, strlen(buf) + strlen(line) + 1);
     buf = strcat(buf, line);
+    //write(fd, line, strlen(line) + 1);
   }
+
+  write(fd, buf, strlen(buf) + 1);
+
+  // Creating patch file
+  // Needs naming convention	
+  char command[2048];
+  strcpy(command, "diff -c .SANDWICH.GOD /tmp/pipe > ");
+
 
   free(buf);
 
-  unlink(myfifo);
+  //unlink(myfifo);
   close(fd);
 
   return 0;
