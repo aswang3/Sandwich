a0b6a4795d8d89074b95949d408320cc4d66c911
Pooja <pgwelling@gmail.com>
2016-11-12 17:55:02 -0600

    server updated to execute patch command

--- server.c
+++ server.c
@@ -30,11 +30,17 @@ int main(int argc, char**argv[]) //argv 1 is name of file
 	while(scanf("%s %d", patch, timestamp)!=EOF)
 	{
 
+		printf("patch received\n");
+
 //TODO Perform comparison. If passed, update
 		mastertime = *timestamp;
+		char patch_command [strlen(patch) +15];
+		strcpy(patch_command, "patch < ");
+		strcat(patch_command, patch);
+		system(patch_command);
 
 
-
+		printf("patch applied\n");
 
 	}
 
